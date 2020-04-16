2088f071c1ee95c9be9ec34e3551ac9eea007348
Adam Robertson <adam.robertson13@gmail.com>
2020-04-16 12:34:24 -0600

    cp4

---
+++ front-end/src/App.vue
@@ -0,0 +1,47 @@
+<template>
+  <div id="app">
+    <header id="header">
+      <h1>Creative Project 4</h1>
+    </header>
+    <router-view />
+  </div>
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+#header {
+  /* Semi-circle */
+  margin: 0 1em 1em 0;
+  height: 100px;
+  width: 200px;
+  /* Fixed position */
+  position: fixed;
+  z-index: 10000;
+  left: 50%;
+  transform: translate(-50%, 0);
+  /* Color and alignment */
+  background: green;
+  text-align: center;
+  box-shadow: 0 0 0 1em #FFF;
+}
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
+}
+</style>
